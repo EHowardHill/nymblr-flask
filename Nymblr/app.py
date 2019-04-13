e7b0e47fd575411cabb997cb94df3b74fab1c761
Ethan Hill <ethan.hill1999@outlook.com>
2019-04-13 15:35:20 -0500

    Some ideas

--- Nymblr/app.py
+++ Nymblr/app.py
@@ -1,10 +1,17 @@
 import os, json, mysql.connector as mariadb, pprint, sqlite3
 from datetime import datetime
 from flask import Flask, render_template, request
+from flask_googlelogin import GoogleLogin
+from flask_login import LoginManager
 
+# Initialization
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+googlelogin = GoogleLogin(app, login_manager)
 
 @app.route('/')
+@login_required
 def home():
     return render_template(
         'editor.htm'
@@ -14,3 +21,9 @@ def home():
 def update():
     text =  request.form['text'];
     return text
+
+@app.route('/oauth2callback')
+@googlelogin.oauth2callback
+def create_or_update_user(token, userinfo, **params):
+    print(google_id=userinfo['id'])
+    return redirect(url_for('/'))
