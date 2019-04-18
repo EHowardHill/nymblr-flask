9f25576cd5d97ac49016a1cd1fde8b4fa3368e49
Ethan Hill <ethan.hill1999@outlook.com>
2019-04-18 16:52:31 -0500

    All the new things

--- Nymblr/app.py
+++ Nymblr/app.py
@@ -1,29 +1,15 @@
-import os, json, mysql.connector as mariadb, pprint, sqlite3
-from datetime import datetime
-from flask import Flask, render_template, request
-from flask_googlelogin import GoogleLogin
-from flask_login import LoginManager
+import os, json, pprint
+from flask import Flask, render_template, request, send_from_directory
 
 # Initialization
+UPLOAD_FOLDER = '/txt-uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'fountain'])
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
-googlelogin = GoogleLogin(app, login_manager)
 
 @app.route('/')
-@login_required
 def home():
-    return render_template(
-        'editor.htm'
-        )
+    return render_template('editor.htm')
 
-@app.route('/update', methods=['POST'])
-def update():
-    text =  request.form['text'];
-    return text
-
-@app.route('/oauth2callback')
-@googlelogin.oauth2callback
-def create_or_update_user(token, userinfo, **params):
-    print(google_id=userinfo['id'])
-    return redirect(url_for('/'))
+@app.route('/save', methods=['POST'])
+def save():
+    return send_from_directory('./temp/', 'myfile.txt')
